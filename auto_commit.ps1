356cb633775eefc19783adfe70d971732fe54978
UP3Tolis <rentolitoli@gmail.com>
2025-11-05 17:39:35 +0800

    Create auto_commit.ps1

---
+++ auto_commit.ps1
@@ -0,0 +1,5 @@
+cd "C:\Users\PLN\OneDrive - PLN\002. Perencanaan UP3 TLI\999. DASHBOARD\DASHBOARD"   # <- ganti dengan path kamu
+
+git add .
+git commit -m "Auto update at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" 
+git push
