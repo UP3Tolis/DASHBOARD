ea34d20657231107cfd8ada6694e42854f09d7a1
UP3Tolis <rentolitoli@gmail.com>
2025-11-06 11:36:53 +0800

    Auto update at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')

--- auto_commit.bat
+++ auto_commit.bat
@@ -3,3 +3,7 @@ cd "C:\Users\PLN\OneDrive - PLN\002. Perencanaan UP3 TLI\999. DASHBOARD\DASHBOAR
 git add .
 git commit -m "Auto update at $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" 
 git push
+
+git add --force "NKO UP3 TLI.xlsx"
+git commit -m "Force update excel"
+git push
